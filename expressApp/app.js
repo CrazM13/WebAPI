2c0634df140daefd9275dcd82c20bceeb922e654
Sepulveda <jasepulveda@labs.neit.edu>
2019-01-16 12:15:39 -0500

    Finished Express Site

--- expressApp/app.js
+++ expressApp/app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const path = require("path");
-const http = require("http");
+const router = express.Router();
 
 const app = express();
 
-app.set("views", path.resolve(__dirname, "views"));
-
-app.get("/", function(req, res) {
-    res.render("index.html");
+// Home Page
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
-http.createServer(app).listen(3000, function() {
-    console.log("Server Running On Port 3000");
+// About Page
+router.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/about.html'));
 });
+
+// Contact Page
+router.get('/contact', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/contact.html'));
+});
+
+// Set Up Server
+app.use('/', router);
+app.listen(process.env.port || 3000);
+console.log("Running On Port 3000");
